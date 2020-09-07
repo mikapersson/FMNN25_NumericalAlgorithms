673fe8c6c814b9987fce4cb45e396e31632675e9
mikapersson <perssonmika@gmail.com>
2020-09-07 20:44:49 +0200

    first version basis_function complete

--- Project1/basis_function.py
+++ Project1/basis_function.py
@@ -1,13 +1,29 @@
 from numpy import *
 
 
-def basis_function(knot_points, j):
+def basis_function(knots, i, k, u):
     """
-    Returns the j:th B-spline basis function (N_j)^3 to the corresponding 'knot_points'
-    :param knot_points: (array)
-    :param j: (int) index of basis function to be returned
-    :return: (array) j:th basis function of 'knot_points'
+    Returns
+    :param knots: (array)
+    :param i: (int)
+    :param k: (int) order of the basis function
+    :param u: (float)
+    :return: (array)
     """
 
-    basis_knots = insert(knot_points, [0, len(knot_points)], [knot_points[0], knot_points[-1]])  # insert u_{-1} and u_{K+1}
+    extended_knots = knots
+    if k == 3:  # if we've just entered the recursion
+        # Add u_{-2}, u_{-1}, u_{K+1} and u_{K+2}
+        extended_knots = insert(extended_knots, [0, 0, len(knots), len(knots)], [knots[0], knots[0], knots[-1], knots[-1]])
 
+    if k == 0:
+        return 1.0 if extended_knots[i-1] <= u < extended_knots[i] else 0.0
+    if extended_knots[i + k - 1] == extended_knots[i-1]:
+        c1 = 0.0
+    else:
+        c1 = (u - extended_knots[i-1]) / (extended_knots[i-1 + k] - extended_knots[i-1]) * basis_function(extended_knots, i, k-1, u)
+    if extended_knots[i + k] == extended_knots[i]:
+        c2 = 0.0
+    else:
+        c2 = (extended_knots[i + k] - u) / (extended_knots[i + k] - extended_knots[i]) * basis_function(extended_knots, i+1, k-1, u)
+    return c1 + c2
--- Project1/main.py
+++ Project1/main.py
@@ -1,8 +1,9 @@
-from Cubic_Spline import *
+from CubicSpline import *
 from basis_function import basis_function
 
 
 def main():
+
     test_control = array(
         [[-17, 9],
          [-40, 20],
@@ -15,10 +16,59 @@ def main():
          [18, -3],
          [40, 17]])
     test_knots = array([0, 1, 2, 3, 4, 5, 6, 9, 11, 12])
-    cubsplin = Cubic_Spline(test_control, test_knots)
-    cubsplin.plot()
+    cubsplin = CubicSpline(test_control, test_knots)
+    #cubsplin.plot()
 
-    # b = basis_function(test_knots, 10)
+
+    CONTROL = [(-12.73564, 9.03455),
+               (-26.77725, 15.89208),
+               (-42.12487, 20.57261),
+               (-15.34799, 4.57169),
+               (-31.72987, 6.85753),
+               (-49.14568, 6.85754),
+               (-38.09753, -1e-05),
+               (-67.92234, -11.10268),
+               (-89.47453, -33.30804),
+               (-21.44344, -22.31416),
+               (-32.16513, -53.33632),
+               (-32.16511, -93.06657),
+               (-2e-05, -39.83887),
+               (10.72167, -70.86103),
+               (32.16511, -93.06658),
+               (21.55219, -22.31397),
+               (51.377, -33.47106),
+               (89.47453, -33.47131),
+               (15.89191, 0.00025),
+               (30.9676, 1.95954),
+               (45.22709, 5.87789),
+               (14.36797, 3.91883),
+               (27.59321, 9.68786),
+               (39.67575, 17.30712)]
+    KNOTS = linspace(0, 1, 26)
+    KNOTS[1] = KNOTS[2] = KNOTS[0]
+    KNOTS[-3] = KNOTS[-2] = KNOTS[-1]
+
+
+    #cubsplin = CubicSpline(CONTROL, KNOTS)
+    # cubsplin.plot()
+    # print(cubsplin(0.2))
+
+    size = 1000
+    xspace = linspace(0, 8, size)
+    nvals = zeros(size)
+    nvals2 = zeros(size)
+    maxval = amax(test_knots)
+    for i in range(0, size):
+        nvals[i] = basis_function(test_knots, 0, 3, i * 8 / size)
+        nvals2[i] = basis_function(test_knots, 1, 3, i * 8 / size)
+
+    print(str(nvals.shape))
+    print(str(xspace.shape))
+
+    plt.plot(xspace, nvals, 'b')
+    plt.plot(xspace, nvals2, 'r')
+    plt.grid()
+    plt.show()
 
 
 main()
