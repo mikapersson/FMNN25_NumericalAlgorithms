c9c30d0f23d6c2ac37355bc9bfc132dd0a6f7425
mikapersson <perssonmika@gmail.com>
2020-09-17 19:48:34 +0200

    task 9 first draft

--- Project2/QuasiNewton.py
+++ Project2/QuasiNewton.py
@@ -34,17 +34,50 @@ class GoodBroyden(QuasiNewton):  # 3.17
         delta_k = x_next - x_k
         gamma_k = self.gradient(x_next) - self.gradient(x_k)
         H_km1 = self.inverted_hessian  # "H k minus 1"
-        H_km1 = H_km1 + outer(delta_k - H_km1@gamma_k, H_km1@gamma_k) / dot((H_km1@delta_k).T, gamma_k)
+        H_k = H_km1 + outer(delta_k - H_km1@gamma_k, H_km1@gamma_k) / dot((H_km1@delta_k).T, gamma_k)
 
-        self.inverted_hessian = H_km1
+        self.inverted_hessian = H_k
 
 
-"""
 class BadBroyden(QuasiNewton):  # 3.18
+    def update_hessian(self, x_next, x_k):
+        delta_k = x_next - x_k
+        gamma_k = self.gradient(x_next) - self.gradient(x_k)
+        H_km1 = self.inverted_hessian  # "H k minus 1"
+        H_k = H_km1 + outer((delta_k- H_km1@gamma_k)/inner(gamma_k, gamma_k), gamma_k)
 
-class SymmetricBroyden(QuasiNewton):  # 3.19-3.20
+        self.inverted_hessian = H_k
+
+
+class SymmetricBroyden(QuasiNewton):  # 3.20
+    def update_hessian(self, x_next, x_k):
+        delta_k = x_next - x_k
+        gamma_k = self.gradient(x_next) - self.gradient(x_k)
+        H_km1 = self.inverted_hessian  # "H k minus 1"
+        u = delta_k - H_km1@gamma_k
+        a = 1 / inner(u, gamma_k)
+        H_k = H_km1 + a*outer(u, u)
+
+        self.inverted_hessian = H_k
+
 
 class DFP(QuasiNewton):  # 3.21
+    def update_hessian(self, x_next, x_k):
+        delta_k = x_next - x_k
+        gamma_k = self.gradient(x_next) - self.gradient(x_k)
+        H_km1 = self.inverted_hessian  # "H k minus 1"
+        H_k = H_km1 + outer(delta_k, delta_k)/inner(delta_k, gamma_k) - (H_km1@gamma_k@inner(gamma_k, H_km1))/(inner(gamma_k, H_km1)@gamma_k)
+
+        self.inverted_hessian = H_k
+
 
 class BFGS(QuasiNewton):  # 3.22
-"""
+    def update_hessian(self, x_next, x_k):
+        delta_k = x_next - x_k
+        gamma_k = self.gradient(x_next) - self.gradient(x_k)
+        H_km1 = self.inverted_hessian  # "H k minus 1"
+        H_k = H_km1 + (1 + (inner(gamma_k, H_km1)@gamma_k)/inner(delta_k, gamma_k))*outer(delta_k, delta_k)/inner(delta_k, gamma_k) - \
+            (outer(delta_k, gamma_k)@H_km1 + H_km1@outer(gamma_k, delta_k))/inner(delta_k, gamma_k)
+
+        self.inverted_hessian = H_k
+
